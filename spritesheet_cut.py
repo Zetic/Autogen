387e4c98870002e626be714cf50c0037e0562e53
Cody <Jonesccody@gmail.com>
2023-08-14 05:15:38 -0400

    Fixed spritesheet cutter

--- spritesheet_cut.py
+++ spritesheet_cut.py
@@ -47,6 +47,6 @@ def rename_and_move_files_using_json(pokemon_folder, json_file):
 if __name__ == "__main__":
     sprite_sheet_file = "sprites.png"
     rows = 21
-    cols = 31
+    cols = 21
     split_sprite_sheet(sprite_sheet_file, rows, cols)
     rename_and_move_files_using_json("Pokemon", "pokedex_if_list.json")
